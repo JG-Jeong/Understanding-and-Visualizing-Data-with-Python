18f6f4620e04495312179e3eba538ce115faa2a8
Jeong Jeong Flourance <jo128g@naver.com>
2025-09-05 22:13:11 +0900

    nhane data1

---
+++ week2/Univariate data analyses _ NHANES case study/NHANES.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+da = pd.read_csv("nhanes_2015_2016.csv")
+
+print(da.DMDEDUC2.value_counts())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
