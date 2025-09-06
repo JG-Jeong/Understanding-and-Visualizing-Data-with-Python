e31ec4b232d25bf187276d1aea931ec66a4c97b0
Jeong Jeong Flourance <jo128g@naver.com>
2025-09-06 19:32:08 +0900

    NHANES prac_9.6

--- week2/NHANES.py
+++ week2/NHANES.py
@@ -6,6 +6,14 @@ import numpy as np
 da = pd.read_csv("nhanes_2015_2016.csv")
 
 print(da.DMDEDUC2.value_counts())
+# DMDEDUC2
+# 4.0    1621
+# 5.0    1366
+# 3.0    1186
+# 1.0     655
+# 2.0     643
+# 9.0       3
+# Name: count, dtype: int64
 
 
 
